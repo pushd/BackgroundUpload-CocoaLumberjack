bfbc3d4e02afc65ae85e194a9d0fc6a250b83fbf
Eric Jensen <ericcj@gmail.com>
2015-02-16 20:52:57 -0500

    try to get travis to run

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,3 +6,22 @@
 //  Copyright (c) 2014 Eric Jensen. All rights reserved.
 //
 
+#import <XCTest/XCTest.h>
+#import "DDLog.h"
+#import "PDBackgroundUploadLogFileManager.h"
+
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)testInit
+{
+    DDLogVerbose(@"logged something");
+    XCTAssert(YES, @"Pass");
+}
+
+@end
